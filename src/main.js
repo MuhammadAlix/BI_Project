1b6713eb846f8d265b354a68ed371aa15b2c5f5e
Muhammad Ali Yasin <ay003845@gmail.com>
2024-06-04 18:12:38 +0500

    Added syntax highlighting.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// mainbdr.onmouseover = function(e){
+    //     if(e.target !== e.currentTarget) return;
+    //     console.log("border-hover",)
+    // }
+    // function position(event){
+        //     let y = event.clientY;
+        //     let x = event.clientX;
+        //     document.getElementById("seq-text").value=x,y
+        // }
+var mainGrid = document.querySelector('grid');
+var mainbdr = document.getElementById("border1");
+// console.log(mainGrid.style.gridTemplateColumns);
+// console.log(width)
+function resizer(mainbdr,mainGrid){
+    var y,w;
+}
