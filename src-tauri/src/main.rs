8a816df5e3d89fc3a056077927e59a4859d68887
MhammadTalha <139311711+MhammadTalha@users.noreply.github.com>
2024-06-12 23:17:34 +0500

    first tool copleted

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,18 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn n_count(seq: &str) -> String {
+    let newseq = seq.to_uppercase();
+    let count_a = newseq.matches('A').count();
+    let count_t = newseq.matches('T').count();
+    let count_g = newseq.matches('G').count();
+    let count_c = newseq.matches('C').count();
+    format!("Count of A is {}! Count of T is {}! Count of G is {}! Count of C is {}!", count_a, count_t, count_g, count_c)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![n_count])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+const invoke = window.__TAURI__.invoke;
+
+
+
+// resizing terminals
+
+
+// sidebar resizing
+
 var resizeable = document.querySelector('.sidr'),
     sidebar = document.querySelector('.right_sidebar'),
     main = document.querySelector('.main');
@@ -32,6 +41,9 @@ function resizer(sidebar, resizeable, main) {
 
 resizer(sidebar, resizeable, main);
 
+// codearea resizing
+
+
 var codeArea = document.querySelector('.codearea'),
     resizeHandleCode = document.querySelector('.coder');
 
@@ -49,7 +61,7 @@ function codeAreaResizer(codeArea, resizeHandleCode) {
     function rs_mousemoveHandler(e) {
         var dx = e.clientX - x;
         var cw = w - dx; // Update width calculation to resize from left to right
-        if (cw > 100 && cw < window.innerWidth - 100) { // Set minimum and maximum code area width
+        if (cw > 200 && cw < window.innerWidth - 800) { // Set minimum and maximum code area width
             codeArea.style.width = `${cw}px`;
         }
     }
@@ -64,6 +76,9 @@ function codeAreaResizer(codeArea, resizeHandleCode) {
 
 codeAreaResizer(codeArea, resizeHandleCode);
 
+
+
+// terminal resizing
 var terminal = document.querySelector('.terminal'),
     resizeHandle = document.querySelector('.terminalr');
 
@@ -95,3 +110,135 @@ function terminalResizer(terminal, resizeHandle) {
 }
 
 terminalResizer(terminal, resizeHandle);
+
+
+// sidebar click functionalities
+
+document.addEventListener("DOMContentLoaded", function() {
+    const DNA = document.querySelector('.DNA');
+    const DNA_data = document.querySelector('.DNA-data');
+    const RNA = document.querySelector('.RNA');
+    const RNA_data = document.querySelector('.RNA-data');
+
+    DNA.addEventListener('click', function() {
+        if (DNA_data.style.display === "none") {
+            DNA_data.style.display = "block";
+            RNA_data.style.display = "none";
+        } else {
+            DNA_data.style.display = "none";
+        }
+    });
+    RNA.addEventListener('click', function() {
+        if (RNA_data.style.display === "none") {
+            RNA_data.style.display = "block";
+            DNA_data.style.display="none";
+        } else {
+            RNA_data.style.display = "none";
+        }
+    });
+});
+
+
+// input area click functionalities
+
+document.addEventListener("DOMContentLoaded",function(){
+    const code = document.querySelector("#codebtn");
+    const codedata = document.querySelector(".codearea");
+    const reset = document.querySelector("#resetbtn");
+    const textarea = document.querySelector("#seq-text");
+
+
+    code.addEventListener('click',function(){
+        if(codedata.style.display==="none"){
+            codedata.style.display = "flex";
+        }else {
+            codedata.style.display = "none";
+        }
+    });
+
+    reset.addEventListener('click',function(){
+        textarea.value = '';
+    });
+
+
+});
+
+
+// Codearea click events
+
+document.addEventListener('DOMContentLoaded', function() {
+    const rust = document.querySelector('#rust');
+    const python = document.querySelector('#python');
+    const rust_code = document.querySelector('#rustCode');
+    const python_code = document.querySelector('#pythonCode');
+    const copy = document.querySelector('#copybtn');
+
+    rust.addEventListener('click', function() {
+        if (rust_code.style.display === 'none') {
+            rust_code.style.display = 'block';
+            python_code.style.display = 'none';
+            rust.style.backgroundColor = '#e6edf3';
+            rust.style.border = '1px solid #e6edf3';
+            rust.style.color = '#656d76';
+            python.style.backgroundColor = '#656d76';
+            python.style.border = '1px solid #656d76';
+            python.style.color = '#e6edf3';
+        } else {
+            rust_code.style.display = 'none';
+        }
+    });
+
+    python.addEventListener('click', function() {
+        if (python_code.style.display === 'none') {
+            python_code.style.display = 'block';
+            rust_code.style.display = 'none';
+            python.style.backgroundColor = '#e6edf3';
+            python.style.border = '1px solid #e6edf3';
+            python.style.color = '#656d76';
+            rust.style.backgroundColor = '#656d76';
+            rust.style.border = '1px solid #656d76';
+            rust.style.color = '#e6edf3';
+        } else {
+            python_code.style.display = 'none';
+        }
+    });
+
+    copy.addEventListener('click', function() {
+        if (python_code.style.display === "block") {
+            navigator.clipboard.writeText(python_code.innerText)
+                .then(() => {
+                    console.log('Python code copied to clipboard');
+                })
+                .catch(err => {
+                    console.error('Failed to copy Python code: ', err);
+                });
+        } else {
+            navigator.clipboard.writeText(rust_code.innerText)
+                .then(() => {
+                    console.log('Rust code copied to clipboard');
+                })
+                .catch(err => {
+                    console.error('Failed to copy Rust code: ', err);
+                });
+        }
+    });
+});
+
+
+
+// submit event
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const submit = document.querySelector('.submit');
+    const terminal = document.querySelector('.terminal');
+
+    submit.addEventListener('click', function() {
+        var textarea = document.querySelector('#seq-text');
+        var output = document.querySelector('.outputss');
+        var text = textarea.value;
+        invoke('n_count', {seq: text}).then((result) => output.textContent=result);
+        terminal.style.display = terminal.style.display === 'none' ? 'block' : 'none';
+    });
+});
+
